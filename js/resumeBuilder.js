95a221869a259bf89b6dc7e1b974bccb74c4baf9
Fringilla0 <fringilla0@gmail.com>
2016-02-14 23:00:53 +0100

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -3,8 +3,20 @@ This is empty on purpose! Your code to build the resume will go here.
 */
 var name = "Fringilla";
 var role = "birdwatcher";
+var email = "fringilla0@gmail.com";
+
 var formattedName = HTMLheaderName.replace("%data%", name);
 var formattedRole = HTMLheaderRole.replace("%data%", role);
+var formattedEmail = HTMLemail.replace("%data%", email);
+
 $("#header").append(formattedName);
 $("#header").append(formattedRole);
 
+var bio = { "name" : "Fringilla", 
+            "role" : "birdwatcher", 
+            "contacts" : { "email" : "fringilla0@gmail.com", 
+                           "twitter" : "@fringilla0" },
+            "bioPic" : "images/fry.jpg", 
+            "welcomeMsg" : "Nazdar." }
+
+$("#header").append(bio);

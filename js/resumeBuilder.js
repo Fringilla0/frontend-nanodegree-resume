3075c1bfd847f834c3ac948faf83b397b30e6da9
Fringilla0 <fringilla0@gmail.com>
2016-02-14 20:55:31 +0100

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,5 +1,10 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
- */
- $("#main").append("Fringilla");
+*/
+var name = "Fringilla";
+var role = "birdwatcher";
+var formattedName = HTMLheaderName.replace("%data", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+$("#header").append(formattedName);
+$("#header").append(formattedRole);
 

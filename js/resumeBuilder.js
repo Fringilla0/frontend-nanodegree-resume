84070efb6a321ee20a17d273bea177b52a508db1
Fringilla0 <fringilla0@gmail.com>
2016-02-22 00:39:59 +0100

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -12,14 +12,4 @@ var formattedEmail = HTMLemail.replace("%data%", email);
 $("#header").append(formattedName);
 $("#header").append(formattedRole);
 
-var skills = ["understanding speech of birds", "recognizing bird species"];
-
-var bio = { "name" : "Fringilla", 
-            "role" : "birdwatcher", 
-            "contacts" : { "email" : "fringilla0@gmail.com", 
-                           "twitter" : "@fringilla0" },
-            "bioPic" : "images/fry.jpg", 
-            "welcomeMsg" : "Nazdar.",
-            "skills" : skills };
-
-$("#header").append(bio);
+$("#header").append(formattedEmail);
